7d8446e06e18ea435a3ee0d84911cc31098a610b
Max Baumann <info@fosefx.com>
2021-08-18 11:22:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nextcloud-desktop
+
+This is an npm-mirror of [nextcloud/desktop](https://github.com/nextcloud/desktop)'s AppImages and is subsequently [GPL-2.0-or-later licensed](https://github.com/nextcloud/desktop#scroll-license).
+
+## Usage:
+
+Install:
+> npm i -g nextcloud-desktop
+
+Run:
+> nextcloud-desktop
---
+++ index.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+
+const { Octokit } = require("@octokit/rest");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const { spawn } = require("child_process");
+
+const repoInfo = {
+  owner: "nextcloud",
+  repo: "desktop",
+};
+
+const octokit = new Octokit();
+
+async function checkAndUpdate() {
+  const currentVersion = getCurrentVersion();
+  const release = await octokit.rest.repos.getLatestRelease(repoInfo);
+  const releaseVersion = release.data.tag_name;
+  if (releaseVersion === currentVersion) {
+    return;
+  }
+  const appImageAsset = release.data.assets.find(({ name }) =>
+    name.endsWith(".AppImage")
+  );
+  if (!appImageAsset) {
+    throw new Error("New release detected, but AppImage assets not found");
+  }
+  const url = appImageAsset.browser_download_url;
+  makeSureBinExists();
+  removeOld();
+  await download(url);
+  writeVersion(releaseVersion);
+}
+
+function getCurrentVersion() {
+  try {
+    const file = fs.readFileSync("./bin/version.txt", { encoding: "utf-8" });
+    return file.trim();
+  } catch (e) {
+    if (e.code && e.code === "ENOENT") {
+      return "none";
+    }
+    throw e;
+  }
+}
+
+function makeSureBinExists() {
+  try {
+    fs.mkdirSync("./bin");
+    return;
+  } catch (e) {
+    if (e.code && e.code === "EEXIST") {
+      return;
+    }
+    throw e;
+  }
+}
+
+function removeOld() {
+  try {
+    fs.unlinkSync("./bin/nextcloud.AppImage");
+  } catch (e) {
+    if (e.code && e.code === "ENOENT") {
+      return;
+    }
+    throw e;
+  }
+}
+
+function download(url) {
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Download failed", res);
+      }
+      return res.body;
+    })
+    .then((stream) => {
+      return new Promise((resolve, reject) => {
+        const fStream = fs.createWriteStream("./bin/nextcloud.AppImage");
+        stream.pipe(fStream);
+        fStream.on("error", (e) => reject(e));
+        fStream.on("close", (e) => {
+          resolve();
+        });
+      });
+    })
+    .then(() => {
+      fs.chmodSync("./bin/nextcloud.AppImage", 0o775);
+    });
+}
+
+function writeVersion(newVersion) {
+  fs.writeFileSync("./bin/version.txt", newVersion, { encoding: "utf-8" });
+}
+
+checkAndUpdate()
+  .then(() => {
+    spawn("./bin/nextcloud.AppImage", { detached: true }).unref();
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
